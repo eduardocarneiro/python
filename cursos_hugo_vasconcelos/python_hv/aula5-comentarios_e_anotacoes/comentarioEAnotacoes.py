33ccc795dd833977f3356fba36f6949f88e70cb9
Eduardo Carneiro <eduardo.oc@gmail.com>
2021-01-07 01:22:32 -0300

    adicionado aula5

---
+++ cursos_hugo_vasconcelos/python_hv/aula5-comentarios_e_anotacoes/comentarioEAnotacoes.py
@@ -0,0 +1,11 @@
+# Isto aqui eh uma anotacao
+print("EOC");
+
+"""
+Isto aqui eh comentario tb
+outra linha no mesmo comentario
+aqui eh mais uma e pode ser usada quantas quiser
+"""
+
+print("nome:", "Eduardo O. Carneiro","\nIdade:",35,"\n" );
+
