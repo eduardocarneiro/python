db98ad7a6c8e3bfb13939f2e1089c3b5e9e0be9f
Eduardo Carneiro <eduardo.oc@gmail.com>
2021-01-08 01:36:44 -0300

    adicionado aula7

---
+++ cursos_hugo_vasconcelos/python_hv/aula7-calculo_com_variaveis/calculoComVariaveis.py
@@ -0,0 +1,18 @@
+
+a = b = c = 20;
+
+print("Aluno1 tem:", a, "anos de idade")
+print("Aluno2 tem:", b, "anos de idade")
+print("Aluno3 tem:", c, "anos de idade")
+
+soma = a + b
+sub = soma - c
+div = c - b
+multi = soma * a
+pot = b**3
+
+print("A soma de",a,"+",b,"=",soma)
+print("A subtracao de",soma,"-",c,"=",sub)
+print("A divisao de",c,"/",b,"=",div)
+print("A multiplicacao de",soma,"*",a,"=",multi);
+print("A potencia de",b,"**3","=",pot);
