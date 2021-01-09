f713daf3093cfda6df9e51eb9784ad2b36333550
Eduardo Carneiro <eduardo.oc@gmail.com>
2021-01-09 01:29:39 -0300

    adicionado aula9

---
+++ cursos_hugo_vasconcelos/python_hv/aula9-convertendo_resultado_em_inteiro/convertendoResultadoEmInteiro.py
@@ -0,0 +1,16 @@
+x = input("Digite um valor: ")
+
+print(x)
+
+##
+# convertendo valor da variavel para inteiro
+
+idade = int(input("Digite sua idade: "))
+print("Eu tenho",idade, "anos de idade")
+
+
+##
+# convertendo para inteiro outra opcao
+idade2 = input("Digita sua idade novamente: ")
+int(idade2)
+print("Minha idade eh:", idade2)
