c8fdc55ed5abd785e37cdd99cd74784e62456bc2
Eduardo Carneiro <eduardo.oc@gmail.com>
2021-01-09 20:50:48 -0300

    adicionado aula10

---
+++ cursos_hugo_vasconcelos/python_hv/aula10-sistema_de_calculo/sistemaDeCalculo.py
@@ -0,0 +1,15 @@
+# Sistema de Calculo
+
+"""
+by EOC
+Simple system calc
+"""
+
+x = int(input("Digite o primeiro valor: "))
+y = int(input("Digite o segundo valor: "))
+
+print("A soma de", x, "+", y, "e: ", x + y)
+print("A subtracao de", x, "-", y, "e: ", x -y)
+print("A multiplicacao de", x, "*", y, "e: ", x * y)
+print("A divisao de", x, "-", y, "e: ", x - y )
+
