33f88ae438859d176ddb3f5b3d447ecc4754f319
Eduardo Carneiro <eduardo.oc@gmail.com>
2021-02-06 00:19:33 -0300

    adicionado aul13

---
+++ cursos_hugo_vasconcelos/python_hv/aula13-if_e_composto/ifEComposto.py
@@ -0,0 +1,10 @@
+
+idade = int(input("Digite sua idade: "))
+
+if idade >= 20:
+    if idade <= 60:
+        print("idade entre 20 e 60 anos")
+    else:
+        print("idade NAO esta entre 20 e 60 anos")
+else:
+    print("idade NAO esta entre 20 e 60 anos")
