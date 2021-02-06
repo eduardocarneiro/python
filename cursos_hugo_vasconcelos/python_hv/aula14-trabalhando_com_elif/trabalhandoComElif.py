6630c71b106fd1b0ae9d597003dc86b8d3e087e4
Eduardo Carneiro <eduardo.oc@gmail.com>
2021-02-06 00:32:17 -0300

    adicionado aula14

---
+++ cursos_hugo_vasconcelos/python_hv/aula14-trabalhando_com_elif/trabalhandoComElif.py
@@ -0,0 +1,19 @@
+
+semana = int(input("digite um numero entre 1 - 7: "))
+
+if semana == 1:
+    print("Domingo")
+elif semana == 2:
+    print("Segunda")
+elif semana == 3:
+    print("Terca")
+elif semana == 4:
+    print("Quarta")
+elif semana == 5:
+    print("Quinta")
+elif semana == 6:
+    print("Sexta")
+elif semana == 7:
+    print("Sabado")
+else:
+    print("numero invalido")
