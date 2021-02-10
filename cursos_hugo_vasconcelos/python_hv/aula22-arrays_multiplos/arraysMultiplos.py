45cf35cc95441590fa951295c8df8e8ae5bed6a1
Eduardo Carneiro <eduardo.oc@gmail.com>
2021-02-09 22:22:46 -0300

    adicionado aula22

---
+++ cursos_hugo_vasconcelos/python_hv/aula22-arrays_multiplos/arraysMultiplos.py
@@ -0,0 +1,9 @@
+
+lista = ["Laranja", "Abacaxi", "Banana",["nanica", "da terra"]]
+
+# imprimindo o segundo array
+print(lista[3])
+
+
+# imprimindo a segunda posicao do segundo array
+print(lista[3][1])
