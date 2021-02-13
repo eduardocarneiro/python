cb04b63b9705a8f71a54c90d21e6d2616d52983a
Eduardo Carneiro <eduardo.oc@gmail.com>
2021-02-13 11:34:15 -0300

    update aula25

--- cursos_hugo_vasconcelos/python_hv/aula25-praticando-lista-e-arrays/praticandoListaEArrays.py
+++ cursos_hugo_vasconcelos/python_hv/aula25-praticando-lista-e-arrays/praticandoListaEArrays.py
@@ -6,6 +6,8 @@ notas = []
 for i in range(1, alunos + 1):
     notas = 0
     for j in range(1, 5):
-        notas = float(input("Digite a nota[%i/4] do aluno[%i/%i]: " %(j, i, alunos)))
+        notas += float(input("Digite a nota[%i/4] do aluno[%i/%i]: " %(j, i, alunos)))
+    
+    print(notas)
 
-print(notas)
+#print(notas)
