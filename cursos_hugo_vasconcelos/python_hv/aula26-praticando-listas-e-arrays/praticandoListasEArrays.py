7b52729e14840ecb1430640f51bf8bc73e76fdcc
Eduardo Carneiro <eduardo.oc@gmail.com>
2021-02-22 23:50:13 -0300

    adicionado aula26 - estrutura do programa

---
+++ cursos_hugo_vasconcelos/python_hv/aula26-praticando-listas-e-arrays/praticandoListasEArrays.py
@@ -0,0 +1,16 @@
+
+"""
+4 notas de 2 alunos, calcule e grave a media em um vetor para cada aluno, imprir o numero de alunos com media >=6
+
+"""
+
+alunos = 2
+
+media = []
+
+for listaAlunos in range(1, alunos +1):
+    notas = 0
+    print(listaAlunos)
+    for notasAlunos in range(1, 5):
+        print(notasAlunos)
+
