e16395542bddccf782638222fa48fdfdc551ed23
Eduardo Carneiro <eduardo.oc@gmail.com>
2021-02-23 00:40:57 -0300

    adicionado aula26 completa

--- cursos_hugo_vasconcelos/python_hv/aula26-praticando-listas-e-arrays/praticandoListasEArrays.py
+++ cursos_hugo_vasconcelos/python_hv/aula26-praticando-listas-e-arrays/praticandoListasEArrays.py
@@ -1,16 +1,21 @@
-
-"""
-4 notas de 2 alunos, calcule e grave a media em um vetor para cada aluno, imprir o numero de alunos com media >=6
-
-"""
+#!/usr/bin/python
 
 alunos = 2
 
 media = []
 
-for listaAlunos in range(1, alunos +1):
+for i in range(1, alunos +1):
     notas = 0
-    print(listaAlunos)
-    for notasAlunos in range(1, 5):
-        print(notasAlunos)
+    for j in range(1, 5):
+        print("Digite a nota %i para o aluno %i: " %(j,i))
+        notas += float(input())
+    notas /= 4
+    media.append(notas)
 
+n = 0 
+
+print(media)
+for mediaAluno in media:
+    if mediaAluno >= 6:
+        n += 1
+print(n) 
