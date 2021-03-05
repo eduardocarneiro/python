5e72b1fb4a41042b0b7dd6ac25445310b1ff3649
Eduardo Carneiro <eduardo.oc@gmail.com>
2021-03-04 23:41:56 -0300

    added dns tests on registerDNS.py file

--- phpipam/registerDns.py
+++ phpipam/registerDns.py
@@ -2,15 +2,53 @@
 # coding=UTF-8
 
 import paramiko
+import os
 from datetime import date
+
+
+zone = "ns1.devops.lab.zone"
 
 host = '10.11.58.11'
 username='root'
-password='password'
+password='P@ssw0rd'
+
+
+# check and allocated a free dns hostname
+#import os
+
+serverPreffix = "srv"
+serverSuffix = ".devops.lab"
+dnsServer = "10.11.58.11"
+
+count = 1
+for count in range(110,230):
+    hostname = serverPreffix + str(count) + serverSuffix
+    print(hostname)
+    returnedValue =  os.system('nslookup ' + hostname + dnsServer + ' &> /dev/null ')
+    print(returnedValue)
+    if returnedValue == 0:
+        print("Nome indisponivel")
+    else:
+        print(hostname + " esta disponivel pra uso")
+        hostnamePreffix = str(serverPreffix + str(count))
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(hostname=host, username=username, password=password)
+        #stdin, stdout, stderr = ssh.exec_command("echo -e " + "'" + hostnamePreffix + "\t\t" + "IN" + "\t" + "A" + "10.11.58.252" + zone + "'")
+        stdin, stdout, stderr = ssh.exec_command("cat /var/named/ns1.devops.lab.zone")
+        for out in stdout:
+            print(out.strip('\n'))
+        ssh.close()
+        break
+
+"""
+
+
+
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh.connect(hostname=host, username=username, password=password)
-stdin, stdout, stderr = ssh.exec_command("grep -i serial /var/named/ns1.devops.lab.zone | awk '{ print $1}'")
+stdin, stdout, stderr = ssh.exec_command("grep -i serial /var/named/" + zone +  | awk "'{ print $1}'")
 
 todayDate = date.today()
 
@@ -33,6 +71,22 @@ for currentRemoteBindSerialNumber in stdout:
 
 ssh.close()
 
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(hostname=host, username=username, password=password)
+stdin, stdout, stderr = ssh.exec_command("grep -i serial /var/named/" + zone +  | awk "'{ print $1}'")
+
+todayDate = date.today()
+
+server = "server01.devops.lab"
+
+ssh.close()
+
+
+
+
+
+
 #stdin, stdout, stderr = ssh.exec_command("sed -i 's/' /var/named/ns1.devops.lab.zone")
 
 #ssh.close()
@@ -41,6 +95,7 @@ ssh.close()
 #        print(line.strip('\n'))
 #ssh.close()
 
+"""
 """
 all_lines = ''
 for line in stdout.readlines():
@@ -57,5 +112,6 @@ reference:
     https://www.tutorialspoint.com/python/python_numbers.htm
     https://stackoverflow.com/questions/5863999/python-cut-example
     https://www.unix.com/shell-programming-and-scripting/270693-sed-command-inside-paramiko-python.html
+    https://stackoverflow.com/questions/5863999/python-cut-example
 
 """
